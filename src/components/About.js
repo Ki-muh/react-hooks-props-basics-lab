9ddb2084761f1977eb4a27d7bd898ee1452f38b1
Martin Kamau <martinkimu09@gmail.com>
2022-09-26 13:06:07 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,17 +2,18 @@ import React from "react";
 import Links from "./Links"
 
 function About(props) {
-  if(!props.bio){
-    return (null);
-  }
-  else{return (
-    <div id="about">
+  if(props.bio){
+    return (
+      <div id="about">
       <h2>About Me</h2>
       <p>{props.bio}</p> 
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links linkedin={props.links.linkedin} github={props.links.github} />
     </div>
-  );
+
+    );
+  }
+  else{return (" ");
   };
 };
 
